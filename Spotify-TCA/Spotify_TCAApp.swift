6f4933d9095d10baaa0eedd0c42cd75099996575
sownhere <sontran.retech@gmail.com>
2025-02-16 16:12:52 +0700

    Initial Commit

---
+++ Spotify-TCA/Spotify_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Spotify_TCAApp.swift
+//  Spotify-TCA
+//
+//  Created by SownFrenky on 2/16/25.
+//
+
+import SwiftUI
+
+@main
+struct Spotify_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
